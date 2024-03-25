98b70af4e51c25bd68adfb538d9873b8187b6107
Henrique de Oliveira Gomes <henrique2801oliveira@gmail.com>
2024-03-25 14:58:34 -0300

    Modificando a interação da página

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 //DECLARANDO AS VARIAVEIS
 let p1=prompt("Digite seu nome");
-let p2=prompt("O que você gostaria de ter conquistar?");
+let p2=prompt("O que você gostaria de conquistar?");
 let p3=prompt("Em uma escala de 1 a 5, o quanto você irá se dedicar para conquistar tal desejo?");
 let msg=document.getElementById("msg");
 
+while (p3 < 1 || p3 > 5) {
+    p3 = parseInt(prompt("Por favor, insira um valor entre 1 e 5 para sua dedicação:"));
+}
+ 
+
+let RES = ""
+
+
 if (p3 == 1)
 {
-    msg.innerHTML=`<p>Olá, ${p1}. Gostamos do seu desejo de ter conquistar ${p2}, 
-    mas aparentemente você não faz muita questão de conseguir...`
-}else if (p3 == 2)
+    RES = "mas aparentemente você não faz muita questão de conseguir..."
+}
+else if (p3 == 2)
 {
-    msg.innerHTML=`<p>Olá, ${p1}. Achamos interessante seu desejo de consquistar ${p2}, e por mais que você
-     não queira muito conseguir, acreditamos que você consiga com muito esforço</p>`
-}else if (p3 == 3){
-    msg.innerHTML=`<p>Olá, ${p1}. Muito legal seu desejo de conquistar ${p2}, e acreditamos que se você se
-     animar mais com seu desejo você irá conseguir!!</p>`
-}else if (p3 == 4){
-    msg.innerHTML=`<p>Olá, ${p1}. Incrivel seu desejo de consquistar ${p2}, corra atrás dele para se orgulhar no futuro!</p>`
+    RES = "e por mais que você não queira muito conseguir, acreditamos que você consiga com muito esforço"
 }
-else if (p3 == 5){
-    msg.innerHTML=`<p>Olá, ${p1}. Seu desejo de conquistar ${p2} é surreal, 
-    com muito esforço e dedicação você irá realizá-lo em breve, até porque força de vontade você tem bastante!!!</p>`
-}else (p3 != 1 || 2 || 3 || 4 || 5){
-    let p3=prompt("Em uma escala de 1 a 5, o quanto você irá se dedicar para conquistar tal desejo?");
+else if (p3 == 3)
+{
+    RES = "e acreditamos que se você se animar mais com seu desejo você irá conseguir!!"
 }
+else if (p3 == 4)
+{
+    RES = "corra atrás dele para se orgulhar no futuro!"
+}
+else if (p3 == 5)
+{
+    RES = "com muito esforço e dedicação você irá realizá-lo em breve, até porque força de vontade você tem bastante!!!" 
+}
+
+
+msg.innerHTML=`<p>Olá, ${p1}. Gostamos do seu desejo de conquistar ${p2}, ${RES}</p>`
+
+
